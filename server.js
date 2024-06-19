b7b0d1e116f24857728d06077251e4b23e8e715a
vinaypanwar123 <vinaypanwar123s@gmail.com>
2024-06-19 13:37:22 +0530

    feat: Setup initial project structure with schemas, Express files, and routes
    - Added Mongoose schemas for user and product models
    
    - Created Express server setup with basic middleware
    
    - Defined initial API routes for user and product operations

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const app = express();
+
+mongoose.connect('mongodb://localhost/urlShortener');
+
+
+
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:false}));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+app.post("/shortUrls",async(req,res)=>{
+   await ShortUrl.create({full:req.body.fullUrl});
+   res.redirect("/");
+})
+
+
+app.listen(process.env.PORT || 5000);
