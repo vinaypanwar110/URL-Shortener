522042b161b2921024697492cfc1af7183daf98c
vinaypanwar123 <vinaypanwar123s@gmail.com>
2024-06-30 19:02:27 +0530

    Change the mongo url

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ShortUrl = require("./models/shortUrl");
+require("dotenv").config();
 const app = express();
 
-mongoose.connect('mongodb://localhost/urlShortener');
+// mongoose.connect("mongodb://localhost/urlShortener");
 
+mongoose.connect(process.env.MONGO_URI);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
 
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended:false}));
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
 
-app.get("/",(req,res)=>{
-    res.render("index");
-})
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) {
+    return res.sendStatus(404);
+  }
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.full);
+});
 
-app.post("/shortUrls",async(req,res)=>{
-   await ShortUrl.create({full:req.body.fullUrl});
-   res.redirect("/");
-})
+app.post("/deleteUrl/:id", async (req, res) => {
+  await ShortUrl.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
 
-
-app.listen(process.env.PORT || 5000);
+const PORT = (process.env.PORT || 5000);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
